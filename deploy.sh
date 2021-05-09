bb7949482f878bf6706fe32a2820e54888ea12dd
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-09 21:13:38 +0300

    Fix execution paths

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+CURRENT_DIRECTORY=$(pwd)
+
 git pull --rebase origin master
 git remote show upstream || git remote add upstream https://github.com/AlainVandermeersch/foodbank_server.git
 git pull --rebase upstream master
@@ -8,8 +10,10 @@ rm -rf target
 mkdir target
 cd target && \
     git clone https://github.com/AlainVandermeersch/foodbank-client.git && \
+    cd foodbank-client && \
     docker build -t foodbank-client .
-cd ..
+
+cd $CURRENT_DIRECTORY
 
 docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
