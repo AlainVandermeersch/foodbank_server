bfb392bc09c4661126911f05461c3f90a4ede297
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-09 20:56:28 +0300

    Create deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-cd target && \
+git pull --rebase origin master
+git remote show upstream || git remote add upstream https://github.com/AlainVandermeersch/foodbank_server.git
+git pull --rebase upstream master
+
+mkdir target && \
+    cd target && \
     git clone git@github.com:AlainVandermeersch/foodbank-client.git && \
     npm run build-prod && \
     docker build -t foodbank-client . && \
     cd ..
-
-git pull --rebase git@github.com:AlainVandermeersch/foodbank_server.git master
 
 docker-compose up -d
