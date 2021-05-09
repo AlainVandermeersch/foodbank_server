1c7c8608b0b1dd1f2afef01df4a24fa646f65067
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-09 21:10:12 +0300

    Split mkdir from the rest of the command

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ git pull --rebase origin master
 git remote show upstream || git remote add upstream https://github.com/AlainVandermeersch/foodbank_server.git
 git pull --rebase upstream master
 
-mkdir target && \
-    cd target && \
+mkdir target
+cd target && \
     git clone git@github.com:AlainVandermeersch/foodbank-client.git && \
     npm run build-prod && \
     docker build -t foodbank-client . && \
