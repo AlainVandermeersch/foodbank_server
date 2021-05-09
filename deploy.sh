c600bb44fe1b89407e621b8fcbde00bf2a28696c
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-09 21:09:17 +0300

    Add env file param

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,4 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
-    docker/compose:1.29.1 up -d
+    docker/compose:1.29.1 --env-file=.env.gcloud-dev up -d
