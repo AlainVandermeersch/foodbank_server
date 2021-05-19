e81a8d35323cd87ac97c05aa4c0a406054519e81
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-19 17:56:04 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -19,4 +19,9 @@ docker run --rm \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v "$PWD:$PWD" \
     -w="$PWD" \
-    docker/compose:1.29.1 --env-file=.env.gcloud-dev up -d
+    docker/compose:1.29.1 --env-file=.env.gcloud-dev up -d --build backend
+docker run --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v "$PWD:$PWD" \
+    -w="$PWD" \
+    docker/compose:1.29.1 --env-file=.env.gcloud-dev up -d frontend
