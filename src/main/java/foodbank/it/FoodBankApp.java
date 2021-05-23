67fdef981dcb8af1736030c1138e48161828b821
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-05-23 21:41:02 +0300

    Fix cors config

--- src/main/java/foodbank/it/FoodBankApp.java
+++ src/main/java/foodbank/it/FoodBankApp.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-
 @SpringBootApplication
 @EnableConfigurationProperties(FoodBankAppConfig.class)
 public class FoodBankApp {
@@ -19,8 +17,7 @@ public class FoodBankApp {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				// registry.addMapping("/**").allowedOrigins(System.getenv("CORS_ALLOWED_ORIGINS"));
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				 registry.addMapping("/**").allowedOrigins(System.getenv("CORS_ALLOWED_ORIGINS"));
 			}
 		};
 	}
