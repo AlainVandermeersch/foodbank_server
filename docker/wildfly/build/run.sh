036630909a63ba44250c102ff6c6f1c6bda54d17
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-11-08 11:54:55 +0200

    Update sidecars and wildfly server

--- docker/wildfly/build/run.sh
+++ docker/wildfly/build/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 ${WILDFLY_HOME}/bin/add-user.sh ${ADMIN_USER} ${ADMIN_PASS}
-
+${WILDFLY_HOME}/bin/jboss-cli.sh --file=bin/adapter-elytron-install-offline.cli
 ${WILDFLY_HOME}/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug *:8787
