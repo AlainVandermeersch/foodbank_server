e53a35891964b465d34f30c940492a041f99f781
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2022-04-14 20:21:55 +0300

    Update deploy script to use sudo when deleting food-it sources

--- deploy.sh
+++ deploy.sh
@@ -38,4 +38,4 @@ docker run --rm \
 cd target/food-it && \
     /bin/bash deploy.sh --libext /home/ubuntu/lib-ext --password $WILDFLY_ADMIN_PASS
 cd $CURRENT_DIRECTORY
-rm -rf target/food-it
+sudo rm -rf target/food-it
